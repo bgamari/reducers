bd5134d0ddc36b5c3c8129764d5d21633e1f4312
Ben Gamari <ben@smart-cactus.org>
2017-09-16 21:44:44 -0400

    fingertree now has a Semigroup instance

--- src/Data/Semigroup/Instances.hs
+++ src/Data/Semigroup/Instances.hs
@@ -8,5 +8,7 @@ module Data.Semigroup.Instances where
 import Data.FingerTree
 import Data.Semigroup
 
+#if !(MIN_VERSION_fingertree(0,1,2))
 instance Measured v a => Semigroup (FingerTree v a) where
   (<>) = mappend
+#endif
